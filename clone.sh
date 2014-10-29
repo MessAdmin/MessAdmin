4b3cb013e3c6a3d07152f67a030d39e68ad2e22b
Cédrik LIME <REMOVE_ME+github@cedrik.fr>
2014-10-29 22:47:30 +0100

    Add GitSlave for easier sub-repos management
    http://gitslave.sourceforge.net
    Version: 2014-02-20

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-for p in AdminWebApp AutoProbe ClickStream Core DiskBrowser Ehcache Hibernate Log4J OSCache Quartz1 Quartz2 ScriptExecutor Serializable ServletStats SessionKiller SizeOf UpdateServer; do git clone git@github.com:javabean/MessAdmin-$p.git; done;
+for p in AdminWebApp AutoProbe ClickStream Core DiskBrowser Ehcache Hibernate Log4J OSCache Quartz1 Quartz2 ScriptExecutor Serializable ServletStats SessionKiller SizeOf UpdateServer; do git clone git@github.com:MessAdmin/MessAdmin-$p.git; done;
 
